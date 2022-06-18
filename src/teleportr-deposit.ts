98126a7acf45ce1c2870bbce0f175ed1f3ef8b18
Ofem Eteng <ofemeteng@gmail.com>
2022-06-18 18:25:57 +0100

    Optimism Deposit Subgraph First Commit

--- src/teleportr-deposit.ts
+++ src/teleportr-deposit.ts
@@ -1,67 +1,19 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  TeleportrDeposit,
-  BalanceWithdrawn,
-  EtherReceived,
-  MaxBalanceSet,
-  MaxDepositAmountSet,
-  MinDepositAmountSet,
-  OwnershipTransferred
+  EtherReceived as EtherReceivedEvent
 } from "../generated/TeleportrDeposit/TeleportrDeposit"
-import { ExampleEntity } from "../generated/schema"
+import { EtherReceived } from "../generated/schema"
 
-export function handleBalanceWithdrawn(event: BalanceWithdrawn): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleEtherReceived(event: EtherReceivedEvent): void {
+  // Create a EtherReceived entity, using the hexadecimal string representation
+  // of the transaction hash as the entity ID
+  let id = event.transaction.hash.toHex()
+  let EtherReceivedEntity = new EtherReceived(id)
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // Set properties on the EtherReceivedEntity, using the event parameters
+  EtherReceivedEntity.depositId = event.params.depositId
+  EtherReceivedEntity.emitter = event.params.emitter
+  EtherReceivedEntity.amount = event.params.amount
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.balance = event.params.balance
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.maxBalance(...)
-  // - contract.maxDepositAmount(...)
-  // - contract.minDepositAmount(...)
-  // - contract.owner(...)
-  // - contract.totalDeposits(...)
+  // Save the EtherReceivedEntity to the store
+  EtherReceivedEntity.save()
 }
-
-export function handleEtherReceived(event: EtherReceived): void {}
-
-export function handleMaxBalanceSet(event: MaxBalanceSet): void {}
-
-export function handleMaxDepositAmountSet(event: MaxDepositAmountSet): void {}
-
-export function handleMinDepositAmountSet(event: MinDepositAmountSet): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
